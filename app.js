be798bd758b5c1aade30cb70077ffe869ff01eb5
Zaarin Anjum <zaarinanjum@iut-dhaka.edu>
2023-02-09 12:16:20 +0600

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('Hello!'));
 
 const port = process.env.PORT || 8082;
 
